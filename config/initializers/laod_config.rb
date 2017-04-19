29c3bd6e5eb810c0c7445c7496a72ed223b2b906
Ahrry <gopalahrry@gmail.com>
2017-04-19 19:45:53 +0200

    add default yml cards

--- config/initializers/laod_config.rb
+++ config/initializers/laod_config.rb
@@ -1,1 +1,2 @@
-APP_CARD_TYPES = YAML.load_file("#{Rails.root.to_s}/config/card_types.yml")[Rails.env.to_s]
+APP_DEFAULT_CARD_TYPES = YAML.load_file("#{Rails.root.to_s}/config/card_types.yml")[Rails.env.to_s]
+APP_DEFAULT_CARDS = YAML.load_file("#{Rails.root.to_s}/config/cards.yml")[Rails.env.to_s]

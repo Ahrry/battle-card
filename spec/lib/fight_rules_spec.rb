44bd81e28af1a8bfac6083c831034e5334a6a33b
Ahrry <gopalahrry@gmail.com>
2017-04-20 00:45:42 +0200

    WIP, test fight rules

--- spec/lib/fight_rules_spec.rb
+++ spec/lib/fight_rules_spec.rb
@@ -4,6 +4,16 @@ require 'rake'
 RSpec.describe FightRules, type: :lib do
 
   before(:each) do
+    CardType.all.destroy
+    APP_DEFAULT_CARD_TYPES.each do |card_type|
+      params = card_type.last
+      FactoryGirl.create :card_type, params
+    end
+
+    Card.all.destroy
+    card_type = CardType.first
+    debugger
+    @card1 = FactoryGirl.create :card, card_type: card_type, level: 89, offensive_capacity: 7, defense_capacity: 5
   end
 
   it "hello", focus: true do

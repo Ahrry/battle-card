770e70a131b48beb1e94f0505d44537056994096
Ahrry <gopalahrry@gmail.com>
2017-04-20 01:00:56 +0200

    remove card in spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,15 +6,5 @@ FactoryGirl.define do
     defense_objects {}
     level 5
   end
-
-  factory :card do
-    sequence(:title, 1) { |n| "MyEvent#{n}"}
-    name ""
-    level 50
-    offensive_capacity 5
-    defense_capacity 5
-    default false
-    association :card_type
-  end
-
+  
 end
--- spec/lib/fight_rules_spec.rb
+++ spec/lib/fight_rules_spec.rb
@@ -9,16 +9,6 @@ RSpec.describe FightRules, type: :lib do
       params = card_type.last
       FactoryGirl.create :card_type, params
     end
-
-    Card.all.destroy
-    card_type = CardType.first
-    debugger
-    @card1 = FactoryGirl.create :card, card_type: card_type, level: 89, offensive_capacity: 7, defense_capacity: 5
-  end
-
-  it "hello", focus: true do
-    debugger
-    puts "hello"
   end
 
 end

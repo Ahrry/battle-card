84a8b25bc3aa7957890f2f282123132f34180883
Ahrry <gopalahrry@gmail.com>
2017-04-27 17:00:15 +0200

    try to fix prod issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User
   validates_uniqueness_of :username
 
   def self.find_or_create(params, game = nil)
-    user = User.where(username: params[:username]).first
-    user = User.create(params) unless user
+    user = User.where(username: params[:username]).first_or_create!
     if game
       game.players << user.id
       game.players.delete(user.id) unless game.save
